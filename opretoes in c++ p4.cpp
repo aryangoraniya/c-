ab0213f3fda5bb2c3e4619429b86bed7eed136e5
aryangoraniya <159221039+aryangoraniya@users.noreply.github.com>
2024-02-08 13:32:08 +0530

    Add files via upload

---
+++ opretoes in c++ p4.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a=5,b=10,c=10;
+	if(a<10||b<a)
+	{
+		cout<<"true\n";
+	}
+	else
+	{
+		cout<<"fales";
+	}
+	
+		if(a<10&&b<a)
+	{
+		cout<<"true\n";
+	}
+	else
+	{
+		cout<<"fales\n";
+}
+
+	if(!a==a)
+	{
+		cout<<"true";
+	}
+	else
+	{
+		cout<<"fales";
+	}
+
+	}
